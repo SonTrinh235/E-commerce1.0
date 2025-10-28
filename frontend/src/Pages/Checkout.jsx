8b9b3fbb41b856d5f269e27e9ddc1e4c82a33f53
DangHoangThanh <thanh.dang2005@hcmut.edu.vn>
2025-10-28 13:06:23 +0700

    Cart and Checkout in Vietnamese & Added utils vnd() & Checkout calls API createOrder

--- frontend/src/Pages/Checkout.jsx
+++ frontend/src/Pages/Checkout.jsx
@@ -1,37 +1,36 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useState } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { CartContext } from "../Context/CartContext";
 import "./CSS/Checkout.css";
+import COD_light from "../assets/COD_light.png";
+import VNPAYLogo from "../assets/Logo-VNPAY-QR.png";
 import DefaultImage from "../assets/placeholder-image.png";
 
-// Import APIs
-import { getCartByUserId } from "../api/cartService";
-import { getProductById } from "../api/productService";
-import { createOrder } from "../api/orderService";
+import { vnd } from "../utils/currencyUtils.js"
+
+// import APIs
+import { createOrder } from "../api/orderService.js";
 
 const Checkout = () => {
+  const { userId } = useContext(ShopContext);
 
-  const {
-    isCartLoading,
-    cartTotal,
-    cartItems,
-    productsLookup,
-  } = useContext(CartContext);
+  const { isCartLoading, cartTotal, cartItems, productsLookup } =
+    useContext(CartContext);
 
-
-  // const { cartItems, all_product, getTotalCartAmount } =
-  //   useContext(ShopContext);
-
-  // cartItems
-  // const itemsInCart = all_product.filter((p) => cartItems[p.id] > 0);
-
-  // state cho form
   const [formData, setFormData] = useState({
     name: "",
     address: "",
     phone: "",
     email: "",
   });
+
+  const orderContent = Object.values(cartItems);
+
+  const checkoutTotalItems = Object.values(cartItems).reduce((total, item) => {
+    return total + item.quantity;
+  }, 0);
+
+  const [selectedPaymentMethod, setSelectedPaymentMethod] = useState("cash");
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -49,59 +48,84 @@ const Checkout = () => {
         formData.address
       }\nPhone: ${formData.phone}\nEmail: ${
         formData.email
-      }\n\nTotal: $${cartTotal.toFixed(2)}`
+      }\n\nTotal: ${vnd(cartTotal)}`
     );
   };
 
+  const placeOrder = async() => {
+    if (!orderContent || !userId || !selectedPaymentMethod) {
+      alert('Order detail missing content');
+      return;
+    }
+    console.log("Checkout: New order");
+    console.log("Checkout: User ID: ", userId);
+    console.log("Checkout: Order content: ", orderContent);
+    console.log("Checkout: Payment method: ", selectedPaymentMethod);
+    alert(`Order plaed: Info logged into console`);
+
+    // Call API
+    try {
+      await createOrder({
+        userId: userId,
+        paymentMethod: selectedPaymentMethod,
+        productsInfo: orderContent,
+      })
+    } catch (error) {
+      console.error("createOrder failed:", error);
+      throw error;
+    }
+
+  };
+
   return (
-    
-    <div className="checkout-page">
-      <h2>Checkout</h2>
+    // Page Container
+    <div className="Checkout-page">
+      <h1>Thanh Toán</h1>
       {true === 0 ? (
         <p>Your cart is empty</p>
       ) : (
-        <div className="checkout-content">
+        // Page Content
+        <div className="Checkout-content">
           {isCartLoading ? (
-          <div>Loading cart ... </div>
-        ) : (
-          // Cart content
-          <div className="checkout-items">
-            {Object.values(cartItems).map((item) => {
-              const currentItemData = productsLookup[item.productId];
-              return (
-                <div className="checkout-item">
-                  <img
-                    src={currentItemData.imageInfo?.url || DefaultImage}
-                    alt={currentItemData.name}
-                  />
-                  <div>
-                    <h3>{currentItemData.name}</h3>
-                    <p>
-                      ${item.price} x {item.quantity} = $
-                      {item.price * item.quantity}
-                    </p>
+            <div>Loading cart ... </div>
+          ) : (
+            // Cart content
+
+            <div className="Checkout-items">
+              <h3>Sản Phẩm Mua:</h3>
+              {Object.values(cartItems).map((item) => {
+                const currentItemData = productsLookup[item.productId];
+                return (
+                  <div key={item.productId} className="Checkout-item">
+                    <img
+                      src={currentItemData.imageInfo?.url || DefaultImage}
+                      alt={currentItemData.name}
+                    />
+                    <div>
+                      <h3>{currentItemData.name}</h3>
+                      <p>
+                        Số lượng {item.quantity} x {vnd(item.price)} = <b>{vnd(item.price * item.quantity)}</b>
+                      </p>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
-        )}
-
-          {/* Right: summary + form */}
-          <div className="checkout-right">
-            <div className="checkout-summary">
-              <h3>Order Summary</h3>
-              <p>Total Items: {Object.keys(cartItems).length}</p>
-              <h3>Total: ${cartTotal}</h3>
+                );
+              })}
             </div>
+          )}
 
-            <div className="checkout-form">
-              <h3>Shipping Information</h3>
+          {/* RIGHT SIDE CHECKOUT */}
+          <div className="Checkout-right">
+
+
+
+            {/* Ship Info */}
+            <div className="Checkout-shipinfo">
+              <h3>Thông Tin Giao Hàng</h3>
               <form onSubmit={handleSubmit}>
                 <input
                   type="text"
                   name="name"
-                  placeholder="Full Name"
+                  placeholder="Họ và Tên"
                   value={formData.name}
                   onChange={handleChange}
                   required
@@ -109,7 +133,7 @@ const Checkout = () => {
                 <input
                   type="text"
                   name="address"
-                  placeholder="Address"
+                  placeholder="Địa Chỉ"
                   value={formData.address}
                   onChange={handleChange}
                   required
@@ -117,7 +141,7 @@ const Checkout = () => {
                 <input
                   type="text"
                   name="phone"
-                  placeholder="Phone Number"
+                  placeholder="Số Điện Thoại"
                   value={formData.phone}
                   onChange={handleChange}
                   required
@@ -130,9 +154,52 @@ const Checkout = () => {
                   onChange={handleChange}
                   required
                 />
-                <button type="submit">Place Order</button>
               </form>
             </div>
+
+
+            {/* Checkout Summary */}
+            <div className="Checkout-summary">
+              <h3>Tổng Quan Đơn Hàng</h3>
+              <p>Tổng số lượng: {checkoutTotalItems} sản phẩm</p>
+              <h3>Tổng thanh toán: {vnd(cartTotal)}</h3>
+            </div>
+
+
+            {/* Payments */}
+            <div className="Checkout-payments">
+              <h3>Chọn phương thức thanh toán: </h3>
+              <div className="option">
+                <label>
+                  <input
+                    type="radio"
+                    name="payment-method"
+                    value="cash"
+                    checked={selectedPaymentMethod === "cash"}
+                    onChange={(e) => setSelectedPaymentMethod(e.target.value)}
+                  />
+                  <img src={COD_light}/>
+                  Thanh toán khi nhận hàng (COD)
+                </label>
+              </div>
+              <div className="option">
+                <label>
+                  <input
+                    type="radio"
+                    name="payment-method"
+                    value="vnpay"
+                    checked={selectedPaymentMethod === "vnpay"}
+                    onChange={(e) => setSelectedPaymentMethod(e.target.value)}
+                  ></input>
+                  <img src={VNPAYLogo}/>
+                  VN Pay
+                </label>
+              </div>
+            </div>
+            {/* Place Order */}
+            <button className="Checkout-placeorder" onClick={placeOrder}>
+              Đặt Hàng
+            </button>
           </div>
         </div>
       )}
