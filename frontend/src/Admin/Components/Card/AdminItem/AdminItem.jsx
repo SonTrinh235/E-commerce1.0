4444aae6977df2cdabe4a681a13859d97683cc3b
DangHoangThanh <thanh.dang2005@hcmut.edu.vn>
2025-11-29 16:33:28 +0700

    Manage products new UI

--- frontend/src/Admin/Components/Card/AdminItem/AdminItem.jsx
+++ frontend/src/Admin/Components/Card/AdminItem/AdminItem.jsx
@@ -3,35 +3,52 @@ import "./AdminItem.css";
 import { Link } from "react-router-dom";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import DefaultImage from "../../../../assets/placeholder-image.png";
-import { vnd } from "../../../../utils/currencyUtils"
+import { vnd } from "../../../../utils/currencyUtils";
 
 const AdminItem = (props) => {
   const { onEdit, onDelete, index } = props;
 
+  const getStockClass = (stock) => {
+    if (stock > 200) return "high";
+    if (stock > 50) return "medium";
+    return "low";
+  };
+
   return (
     <tr className="AdminItem-row">
-      <td id="index">{index}</td>
-      <td id="image">
-        <Link to={`#`}>
-          <img
-            onClick={() => window.scrollTo(0, 0)}
-            src={ props.imageUrl|| props.imageInfo?.url || DefaultImage}
-            alt= {'Image for '+props.name}
-          />
-        </Link>
+      <td>{index}</td>
+      <td>
+        <img
+          onClick={() => window.scrollTo(0, 0)}
+          src={props.imageUrl || props.imageInfo?.url || DefaultImage}
+          alt={"Image for " + props.name}
+          className="product-image"
+        />
       </td>
-      <td id="name">{props.name}</td>
-      <td id="category">
-        <span>
-          {props.category}
+      <td className="product-name-cell">
+        <p className="product-name">{props.name}</p>
+      </td>
+      <td>
+        <span className="category-badge">{props.categoryInfo.name}</span>
+      </td>
+      <td className="product-price">{vnd(props.price)}</td>
+      <td className="product-description-cell">
+        <p className="product-description">{props.description}</p>
+      </td>
+      <td>
+        <span className={`stock-badge ${getStockClass(props.stock)}`}>
+          {props.stock}
         </span>
       </td>
-      <td id="price">{vnd(props.price)}</td>
-      <td id="description">{props.description}</td>
-      <td id="stock">{props.stock}</td>
-      <td id="actions">
-        <button id="edit" onClick={onEdit}><FaEdit/>Chỉnh sửa</button>
-        <button id="delete" onClick={onDelete}><FaTrash/>Xóa SP</button>
+      <td>
+        <div className="action-buttons">
+          <button className="action-btn edit-btn" onClick={onEdit}>
+            <FaEdit fill="white" />
+          </button>
+          <button className="action-btn delete-btn" onClick={onDelete}>
+            <FaTrash fill="white" />
+          </button>
+        </div>
       </td>
     </tr>
   );
